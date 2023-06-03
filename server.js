4503078e3185c18ceb7f8f2c9f488f514d8ce7df
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-06-03 09:40:23 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Parse URL-encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse JSON bodies
+app.use(bodyParser.json());
+
+// Define a route to handle the POST request from the form
+app.post('/submit', (req, res) => {
+  // Access the submitted form data from the request body
+  const { repoName, repoURL, apiKey } = req.body;
+
+  // Log the form data to the console
+  console.log('Repository Name:', repoName);
+  console.log('Repository URL:', repoURL);
+  console.log('API Key:', apiKey);
+
+  // Send a response back to the frontend
+  res.json({ message: 'Form submitted successfully' });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
