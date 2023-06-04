1d97237c8ede0e04aed728eef45f8a21f751524d
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-06-04 00:10:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
 
 // Parse URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
